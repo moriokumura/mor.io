4f333a6265f3a648692656ada3f66e04d0d6255f
Morio Okumura <morio.okumura@gmail.com>
2015-02-11 12:24:38 +0000

    Added rake task.

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -9,4 +9,19 @@ class ListItem < ActiveRecord::Base
   validates :title, presence: true
   validates :url, url: true, allow_blank: true
   
+  # @params [String] title
+  # @returns [String]
+  def get_attribute(title)
+    item = attribute_items.where(title: title).first_or_create
+    item.body
+  end
+  
+  # @params [String] title
+  # @params [String] body
+  def set_attribute(title, body)
+    item = attribute_items.where(title: title).first_or_create
+    item.body = body
+    item.save
+  end
+  
 end
