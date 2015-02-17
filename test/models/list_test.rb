d1371df068ab066ef4eee05573380dd35a3f6924
Morio Okumura <morio.okumura@gmail.com>
2015-02-17 13:44:17 +0000

    Added test for test.

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'normalize_friendly_id' do
+    list = List.new(title: 'Really Awesome List')
+    assert_equal 'Really-Awesome-List', list.normalize_friendly_id(list.title)
+    list = List.new(title: 'Ruby on Rails')
+    assert_equal 'Ruby-on-Rails', list.normalize_friendly_id(list.title)
+    list = List.new(title: 'Google+')
+    assert_equal 'Google-plus', list.normalize_friendly_id(list.title)
+  end
 end
